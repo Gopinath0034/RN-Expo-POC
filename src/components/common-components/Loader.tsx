ffa83089c3993f71563b211b269ec583ea722cf3
gopinathpiku <gopinath.sheet@bipros.com>
2023-08-02 15:52:08 +0530

    add loader

---
+++ src/components/common-components/Loader.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { View, StyleSheet, ActivityIndicator } from "react-native";
+
+// STYLES //
+
+// COMPONENTS //
+
+// SERVICES //
+
+// UTILS //
+
+// PLUGINS //
+
+/** Common Loader */
+const Loader: React.FC<unknown> = () => {
+	return (
+		<View style={styles.container}>
+			<ActivityIndicator size="large" color="#0000ff" />
+		</View>
+	);
+};
+
+export default Loader;
+
+const styles = StyleSheet.create({
+	container: {
+		alignItems: "center",
+		flex: 1,
+		justifyContent: "center",
+	},
+});
